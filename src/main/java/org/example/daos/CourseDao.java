1f7897bf06520a2357c378500f2b135a54a4d10c
Sibora Deda <siboradeda03@gmail.com>
2025-08-23 14:01:40 +0200

    fix course dao

--- src/main/java/org/example/daos/CourseDao.java
+++ src/main/java/org/example/daos/CourseDao.java
@@ -12,39 +12,40 @@ public class CourseDao {
         this.session = session;
     }
 
-    // Shton një kurs të ri në databazë
-    public void save(Course course) {
-        session.beginTransaction();
-        session.persist(course); // ruan kursin
-        session.getTransaction().commit();
-    }
+//    // Shton një kurs të ri në databazë
+//    public void save(Course course) {
+//        session.beginTransaction();
+//        session.persist(course); // ruan kursin
+//        session.getTransaction().commit();
+//    }
+//
+//    // Përditëson të dhënat e një kursi ekzistues
+//    public void update(Course course) {
+//        session.beginTransaction();
+//        session.merge(course); // bashkon ndryshimet me kursin ekzistues
+//        session.getTransaction().commit();
+//    }
+//
+//    // Kthen të gjitha kurset që ekzistojnë në databazë
+//    public List<Course> findAll() {
+//        Query<Course> query = session.createQuery("FROM Course", Course.class);
+//        return query.list();
+//    }
 
-    // Përditëson të dhënat e një kursi ekzistues
-    public void update(Course course) {
-        session.beginTransaction();
-        session.merge(course); // bashkon ndryshimet me kursin ekzistues
-        session.getTransaction().commit();
-    }
 
-    // Kthen të gjitha kurset që ekzistojnë në databazë
-    public List<Course> findAll() {
-        Query<Course> query = session.createQuery("FROM Course", Course.class);
-        return query.list();
-    }
-
-    // Kërkon kurse sipas emrit, klasës ose mësuesit
-    public List<Course> search(String name, String className, String teacherName) {
+    public List<Course> search(String name, String className, String teacherFirstName, String teacherLastName) {
         Query<Course> query = session.createQuery(
-                "FROM Course WHERE name = :name OR className = :className OR teacher.firstName = :teacherName",
-                Course.class);
+                "FROM Course WHERE name = :name OR className = :className OR (teacher.firstName = :teacherFirstName AND teacher.lastName = :teacherLastName)",
+                Course.class
+        );
         query.setParameter("name", name);
         query.setParameter("className", className);
-        query.setParameter("teacherName", teacherName);
+        query.setParameter("teacherFirstName", teacherFirstName);
+        query.setParameter("teacherLastName", teacherLastName);
         return query.list();
     }
+
 }
-
-
 
 
 
